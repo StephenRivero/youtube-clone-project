39ec19e27aaca6235be2d796070d8a9898a5a86e
Stephen Manasseh Rivero Pastor <stephen.m.rivero@gmail.com>
2022-09-10 18:06:45 +0800

    Add ChannelCard, Feed, SearchBar, and Sidebar components
    -Added fetchFromAPI.js file to utils folder

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+    params: {
+      maxResults: '50'
+    },
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    }
+  };
+
+// /baseUrl/getVideos  
+
+export const fetchFromAPI = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+    return data;
+}
