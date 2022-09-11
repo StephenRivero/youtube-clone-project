021b8d1681440fdf97666b6fac0fc5856691eb5d
Stephen Manasseh Rivero Pastor <stephen.m.rivero@gmail.com>
2022-09-11 10:38:15 +0800

    Add ChannelCard, ChannelDetail, & VideoCard

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { demoProfilePicture } from '../utils/constants';
 
 
-const ChannelCard = ({ channelDetail }) => (
+const ChannelCard = ({ channelDetail, marginTop }) => (
     <Box
       sx={{ 
         boxShadow: 'none',
@@ -14,7 +14,8 @@ const ChannelCard = ({ channelDetail }) => (
         alignItems: 'center',
         width: { xs: '356px', md: '320px'},
         height: '326px',
-        margin: 'auto'
+        margin: 'auto',
+        marginTop,
       }}
     >
       <Link to={`/channel/${channelDetail?.id?.channelId}`}>
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -7,18 +7,18 @@ import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoCha
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
   
   return (
-    <Card sx={{ width: { md: '320px', xs: '100%' }, boxShadow: 'none', borderRadius: 0 }}>
+    <Card sx={{ width: { md: '320px', xs: '285px' }, boxShadow: 'none', borderRadius: 0, /*border: '1px solid red'*/ }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia 
-          image={snippet?.thumbnails?.high?.url} 
+          image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} 
           alt={snippet?.title}
-          sx={{ width: 358, height: 180 }}
+          sx={{  height: 160,  }}
         />
       </Link>
-      <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
+      <CardContent sx={{ backgroundColor: '#1e1e1e', width: '95%', height: '80px', display: 'flex', flexDirection: 'column', justifyContent: 'center'  }}>
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
           <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
-            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+            {snippet?.title.slice(0, 70) || demoVideoTitle.slice(0, 70)}
           </Typography>
         </Link>
         <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
