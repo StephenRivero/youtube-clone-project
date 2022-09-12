71117825c9ccb7ae53a93a548e007bc0fec38aab
Stephen Manasseh Rivero Pastor <stephen.m.rivero@gmail.com>
2022-09-12 10:21:03 +0800

    Setup Github Deployment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => (
       p={2} 
       sx={{ position: 'sticky', background: '#000', top:0, justifyContent: 'space-between', display: 'flex'}}
     >
-      <Link to="/" style={{ display: 'flex', alignItems: 'center'}}>
+      <Link to="/youtube-clone-project" style={{ display: 'flex', alignItems: 'center'}}>
         <img src={logo} alt="logo" height={45} />
       </Link>  
       <SearchBar />
